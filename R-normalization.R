3e60d7ff01b18478ac588bc5b6bb9964d2109d8b
Unknown <horrion@me.com>
2022-08-10 13:42:16 +0200

    Add TPM support to R-normalization

--- R-normalization.R
+++ R-normalization.R
@@ -26,9 +26,27 @@ normalized_deseq2_data <- function(input_data) {
   #
 
   ## Create DESeq2Dataset object
-  dds <- DESeqDataSetFromMatrix(countData = input_data)
-  dds <- estimateSizeFactors(dds)
+  dds <- estimateSizeFactorsForMatrix(counts(input_data))
+
+  #dds <- DESeqDataSetFromMatrix(countData = input_data)
+  #dds <- estimateSizeFactors(dds)
   deseq2_normalized_data <- counts(dds, normalized=TRUE)
 
-  return(deseq2_normalized_data)
+  output_matrix <- input_data %*% deseq2_normalized_data
+
+  return(output_matrix)
+}
+
+normalized_tpm_data(input_data, gene_length) {
+  #'
+  #' This function normalizes data using TPM,
+  #' then passes the normalized data back.
+  #'
+  #' @param input_data The input data passed into the function
+  #
+
+  x <- input_data / gene_length
+  tpm <- t( t(x) * 1e6 / colSums(x) )
+
+  return(tpm)
 }
